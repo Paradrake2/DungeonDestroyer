f44e3a155eebc5051f56e94c3f6187c4c9db2ba5
Paradrake2 <57878896+Paradrake2@users.noreply.github.com>
2025-12-29 13:27:36 -0500

    first commit

---
+++ Assets/Scripts/Enemy/EnemyAI/BasicMeleeAI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class BasicMeleeAI : EnemyAI
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
